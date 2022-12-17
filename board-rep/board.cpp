6a2ea4f97f9b653e0ce250cbd5d6e3f8ead96174
joe9go <tnorledge1@gmail.com>
2022-12-17 16:20:13 -0500

    starting testing the board

--- board-rep/board.cpp
+++ board-rep/board.cpp
@@ -2,117 +2,122 @@
 // Created by ajans on 16.12.2022.
 //
 
-#include "board.hpp"
+#include "Board.hpp"
 #include <vector>
 
-    Board::Board(){
-        this->the_board = new Square*[] {new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8]};
-        this->turn = colors::WHITE;
-        this->castle_status = 0b1111;
+Board::Board(void){
+    this->the_board = new Square * [8] {new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8]};
+    this->turn = colors::WHITE;
+    this->castle_status = 0b1111;
+}
+
+Board::Board(Square** squares, colors turn, unsigned int castle_status){
+    this->the_board = squares;
+    this->turn = turn;
+    this->castle_status = castle_status;
+};
+
+Board::~Board() {
+    delete the_board;
+}
+
+void Board::load_board(Board other){
+    this->the_board = other.the_board;
+    this->turn = other.turn;
+}
+
+void Board::load_board(std::string& fen_string){
+    int square_pos = 0;
+    int fen_scan = 0;
+    /*while (tick < 64 && fen_scan < std::strlen(fen_string)){
+
+    }*/
+}
+
+Board* Board::next_from_move(Move move){
+    colors next_turn;
+    Square** next_board;
+
+    switch (this->turn){
+        case colors::WHITE:
+            next_turn = colors::BLACK;
+            break;
+        case colors::BLACK:
+            next_turn = colors::WHITE;
+            break;
+        case colors::NONE:
+            return this;
+            break;
     }
 
-    Board::Board(Square** squares, colors turn, unsigned int castle_status){
-        this->the_board = squares;
-        this->turn = turn;
-        this->castle_status = castle_status;
-    };
+    next_board = board_copy();
+    int_fast8_t next_castle_status;
+    next_castle_status = this->castle_status;
 
-    Board::~Board() {
-        delete the_board;
-    }
+    Square* end_square = &next_board[move.end_x][move.end_y];
+    Square* start_square = &next_board[move.start_x][move.start_y];
 
-    void Board::load_board(Board other){
-        this->the_board = other.the_board;
-        this->turn = other.turn;
-    }
+    colors start_color = start_square->color;
+    pieces start_piece = start_square->piece;
 
-    void Board::load_board(std::string& fen_string){
-        int square_pos = 0;
-        int fen_scan = 0;
-        /*while (tick < 64 && fen_scan < std::strlen(fen_string)){
+    start_square->color = colors::NONE;
+    start_square->piece = pieces::NONE;
 
-        }*/
-    }
-
-    Board* Board::next_from_move(Move move){
-        colors next_turn;
-        Square** next_board;
-
-        switch (this->turn){
-            case colors::WHITE:
-                next_turn = colors::BLACK;
-                break;
-            case colors::BLACK:
-                next_turn = colors::WHITE;
-                break;
+    if(end_square->piece == pieces::EP_GHOST && end_square->color == next_turn){
+        int y;
+        if(next_turn == colors::WHITE){
+            y = move.end_y-1;
         }
-
-        next_board = board_copy();
-        int_fast8_t next_castle_status;
-        next_castle_status = this->castle_status;
-
-        Square* end_square = &next_board[move.end_x][move.end_y];
-        Square* start_square = &next_board[move.start_x][move.start_y];
-
-        colors start_color = start_square->color;
-        pieces start_piece = start_square->piece;
-
-        start_square->color = colors::NONE;
-        start_square->piece = pieces::NONE;
-
-        if(end_square->piece == pieces::EP_GHOST && end_square->color == next_turn){
-            int y;
-            if(next_turn == colors::WHITE){
-                y = move.end_y-1;
-            }
-            if(next_turn == colors::BLACK){
-                y = move.end_y+1;
-            }
-            next_board[move.end_x][y].color = colors::NONE;
-            next_board[move.end_x][y].piece = pieces::NONE;
+        if(next_turn == colors::BLACK){
+            y = move.end_y+1;
         }
-
-        end_square->color = start_color;
-        end_square->piece = start_piece;
-
-        return new Board(next_board, next_turn, next_castle_status);
+        next_board[move.end_x][y].color = colors::NONE;
+        next_board[move.end_x][y].piece = pieces::NONE;
     }
 
-    std::vector<Move> Board::get_legal_moves(){
-        std::vector<Move> legal_moves;
-        for(int pos_x = 0; pos_x < 8; pos_x++){
-            for (int pos_y = 0; pos_y < 8; pos_y++){
-                Move* move = nullptr;
-                switch (this->the_board[pos_x][pos_y].piece){
-                    case pieces::NONE:
-                        break;
-                    case pieces::PAWN:
-                        break;
-                    case pieces::ROOK:
-                        break;
-                    case pieces::KNIGHT:
-                        break;
-                    case pieces::BISHOP:
-                        break;
-                    case pieces::QUEEN:
-                        break;
-                    case pieces::KING:
-                        break;
-                }
-                if(move != nullptr){
-                    legal_moves.push_back(*move);
-                }
-            }
-        }
-        return legal_moves;
-    }
+    end_square->color = start_color;
+    end_square->piece = start_piece;
 
-    Square** Board::board_copy(){
-        Square** copy = new Square * [] {new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8]};
-        for(int x = 0; x < 8; x++){
-            for(int y = 0; y < 8; y++){
-                copy[x][y] = the_board[x][y];
+    return new Board(next_board, next_turn, next_castle_status);
+}
+
+std::vector<Move> Board::get_legal_moves(){
+    std::vector<Move> legal_moves;
+    for(int pos_x = 0; pos_x < 8; pos_x++){
+        for (int pos_y = 0; pos_y < 8; pos_y++){
+            Move* move = nullptr;
+            switch (this->the_board[pos_x][pos_y].piece){
+                case pieces::NONE:
+                    break;
+                case pieces::EP_GHOST:
+                    break;
+                case pieces::PAWN:
+                    break;
+                case pieces::ROOK:
+                    break;
+                case pieces::KNIGHT:
+                    break;
+                case pieces::BISHOP:
+                    break;
+                case pieces::QUEEN:
+                    break;
+                case pieces::KING:
+                    break;
+            }
+            if(move != nullptr){
+                legal_moves.push_back(*move);
             }
         }
-        return copy;
     }
+    return legal_moves;
+}
+
+Square** Board::board_copy(){
+    Square** copy = new Square * [8] {new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8], new Square[8]};
+    for(int x = 0; x < 8; x++){
+        for(int y = 0; y < 8; y++){
+            copy[x][y] = the_board[x][y];
+        }
+    }
+    return copy;
+}
--- board-rep/board.hpp
+++ board-rep/board.hpp
@@ -8,6 +8,13 @@
 #ifndef MENELIK_BOARD_H
 #define MENELIK_BOARD_H
 
+struct Move {
+    int start_x;
+    int start_y;
+    int end_x;
+    int end_y;
+    bool castle = false;
+};
 
 enum class colors {BLACK, WHITE, NONE}; //Defines colors which are linked to a square
 enum class pieces {KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN, EP_GHOST, NONE}; //Defines piece on square.
@@ -17,21 +24,15 @@ struct Square {
     pieces piece = pieces::NONE;
 };
 
-struct Move {
-    int start_x;
-    int start_y;
-    int end_x;
-    int end_y;
-    bool castle = false;
-};
 
 class Board {
 
-    Square** the_board;
-    int_fast8_t castle_status;
-    colors turn = colors::NONE;
 
     public:
+
+        Square** the_board;
+        int_fast8_t castle_status;
+        colors turn = colors::NONE;
 
         Board();
         Board(Square** squares, colors turn, unsigned int castle_status);
@@ -46,6 +47,54 @@ class Board {
         Square** board_copy();
 
 };
+
+std::string color_enum_to_name(colors color){
+    switch (color)
+    {
+    case colors::BLACK:
+        return "BLACK";
+        break;
+    case colors::WHITE:
+        return "WHITE";
+        break;
+    case colors::NONE:
+        return "NONE";
+        break;
+    default:
+        return "ERROR";
+        break;
+    }
+}
+
+std::string piece_enum_to_name(pieces piece){
+    switch (piece)
+    {
+    case pieces::KING:
+        return "KING";
+        break;
+    case pieces::BISHOP:
+        return "BISHOP";
+        break;
+    case pieces::QUEEN:
+        return "QUEEN";
+        break;
+    case pieces::ROOK:
+        return "ROOK";
+        break;
+    case pieces::KNIGHT:
+        return "KNIGHT";
+        break;
+    case pieces::PAWN:
+        return "PAWN";
+        break;
+    case pieces::NONE:
+        return "NONE";
+        break;
+    default:
+        return "ERROR";
+        break;
+    }
+}
 
 
 #endif //MENELIK_BOARD_H
