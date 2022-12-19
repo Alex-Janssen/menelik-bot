5af117a64f827703d4a04b444904b98d77d32668
joe9go <tnorledge1@gmail.com>
2022-12-19 00:13:30 -0500

    there i fix it

--- board-rep/board_test.hpp
+++
@@ -1,7 +0,0 @@
-#include "Board.hpp"
-
-class Board_Test
-{
-    public:
-        static void print_board_state(Board*);
-};
--- board-rep/run_tests.bat
+++
@@ -1,7 +0,0 @@
-set name1=board_test
-
-
-g++ %name1%.cpp -w -W -o .\out\%name1%
-
-
-.\out\%name1%.exe
