543cb7a16dac557eb519daf25be95bbdd8d5da52
joe9go <tnorledge1@gmail.com>
2022-12-19 22:50:20 -0500

    we fixed the worst move finder

--- src/eval/Candidate_Node.cpp
+++ src/eval/Candidate_Node.cpp
@@ -23,12 +23,13 @@ Candidate_Node::Candidate_Node(Board* board, std::function<float(Board*)> eval, 
 /// @param board The boardstate whose next move is to be calculated
 /// @param eval An evaluation function which evaluates the board value
 /// @param parent The parent that spawned it.
-Candidate_Node::Candidate_Node(Board* board, std::function<float(Board*)> eval, Candidate_Node* parent){
+Candidate_Node::Candidate_Node(Board* board, std::function<float(Board*)> eval, Candidate_Node* parent, bool find_out){
     this->board = board;
     this->eval = eval;
     this->parent = parent;
     this->depth = parent->get_depth()+1;
     this->max_depth = parent->get_max_depth();
+    this->is_root = find_out;
     if(this->depth < this->max_depth){
         this->children = this->spawn_children();
     }
@@ -36,7 +37,8 @@ Candidate_Node::Candidate_Node(Board* board, std::function<float(Board*)> eval, 
         this->children = std::vector<Candidate_Node*>();
     }
     this->cur_board_val = eval(board);//Evaluate using flexibible function.
-    this->is_root = false;
+}
+Candidate_Node::~Candidate_Node(){
 }
 /// @brief Returns board encapsulated by node
 /// @return Board.
@@ -74,12 +76,9 @@ bool Candidate_Node::is_prospective_calced(){
 /// @brief Returns the optimal move suceeding this node, which would be the optimal opponent move in an inner node and optimal local move for the root.
 /// @return Pointer to best move calculated.
 Candidate_Node* Candidate_Node::get_best_successor(){
-    std::cout << "get best succ called" << std::endl;
     if (!this->is_prospective_calced()){
-        std::cout << "got in the if" << std::endl;
         this->set_prospective_board_val();
     }    
-    std::cout << "get best succ finished" << std::endl;
     return this-> favorite_child;
 }
 
@@ -91,8 +90,8 @@ std::vector<Candidate_Node*> Candidate_Node::spawn_children(){
     std::vector<Move> moves = this->board->get_legal_moves();
     for(Move move : moves){
         Board* associated_board = this->board->next_from_move(move);
-        Candidate_Node new_child = Candidate_Node(associated_board, eval, this);
-        to_return.push_back(&new_child);
+        Candidate_Node* new_child = new Candidate_Node(associated_board, eval, this, false);
+        to_return.push_back(new_child);
     }
     return to_return;
 }
@@ -102,15 +101,16 @@ void Candidate_Node::set_prospective_board_val(){//THIS HAS INBUILT CACHING, BUT
     Candidate_Node* favorite;
     if(abs(this->cur_board_val)>9000){//A king has been taken, don't recurse.
         this->prospective_board_val = cur_board_val;
-        this->favorite_child = nullptr;    
+        this->favorite_child = nullptr; 
     }
     else{
         if(this->is_root){//Root tries to max children
             int max_option = -99999999999;//Arbitrarily small
             Candidate_Node* best_move = nullptr;
             for(Candidate_Node* cand : this->children){//Iterate over every child board, checking if its prospective has been calced. If it has and it is larger, then replace the max with it.
-                if(cand->get_prospective_board_val() > max_option){
-                    max_option = cand->get_prospective_board_val();
+                float possible = cand->get_prospective_board_val();
+                if(possible > max_option){
+                    max_option = possible;
                     best_move = cand;
                 }
             }
