6fd066f1e7ba2577ec0bd074ad4207de7d0a6d8a
Alex-Janssen <92109675+Alex-Janssen@users.noreply.github.com>
2022-12-19 19:30:55 -0600

    Debug

--- src/eval/Eval_Operator.cpp
+++ src/eval/Eval_Operator.cpp
@@ -8,13 +8,15 @@
 #include <string>
 
 void identify_move(Board board){
-    Candidate_Node root = Candidate_Node(&board, std::function(Eval_Functions::evaluate_board_naive), Load_Params::read_eval_params()["maxdepth"]);
+    std::function func = std::function(Eval_Functions::evaluate_board_naive);
+    int max_depth = Load_Params::read_eval_params()["maxdepth"];
+    Candidate_Node root = Candidate_Node(&board, func, max_depth);
     std::cout << root.get_best_successor()->get_board()->to_string();
 }
 
 int main(){
     auto joker = Board();
-    std::string jokerfen = (std::string)("7k/1R6/8/8/8/8/8/R2K4 w - - 0 1");
+    std::string jokerfen = "7k/1R6/8/8/8/8/8/R2K4 w - - 0 1";
     joker.load_board(jokerfen);
     identify_move(joker);
 }
